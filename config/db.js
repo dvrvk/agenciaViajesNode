1e346121b19f8bced29433244f46e76c9078282f
diegovr <diego.vr.dw@gmail.com>
2023-08-23 17:51:45 +0200

    Modificacion variables entorno

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ import Sequelize from 'sequelize';
 import dotenv from 'dotenv/config';
 
 // (nombre de la base de datos, nombre usuario, password,' configuraciones (local, puerto por defecto, mysql porque tambien soporta otras bases, tiende agregar un par de columnas al actualizar columnas,  ))
-const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    host: process.env.DB_HOST, 
-    port: process.env.DB_PORT, 
+const db = new Sequelize(process.env.MYSQLDATABASE, process.env.MYSQLUSER, process.env.MYSQLPASSWORD, {
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT, 
     dialect: 'mysql', 
     define: {
         timestamps: false
