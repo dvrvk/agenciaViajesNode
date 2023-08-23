ca9f87bc90dfa120cbb96be8db11f3fa7f9785d6
diegovr <diego.vr.dw@gmail.com>
2023-08-23 17:37:02 +0200

    Modificación DB_PORT

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv/config';
 // (nombre de la base de datos, nombre usuario, password,' configuraciones (local, puerto por defecto, mysql porque tambien soporta otras bases, tiende agregar un par de columnas al actualizar columnas,  ))
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST, 
-    port: '3306', 
+    port: process.env.DB_PORT, 
     dialect: 'mysql', 
     define: {
         timestamps: false
